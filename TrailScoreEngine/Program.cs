94d46f71221443b18abdb59b37fc4800173dc141
kwes83 <kwes83@interia.pl>
2021-10-27 09:42:49 +0200

    initial commit

---
+++ TrailScoreEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrailScoreEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
